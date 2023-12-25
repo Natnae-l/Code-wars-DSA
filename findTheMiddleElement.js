117fd2f11a791a721efbe14ef0ded205262dd7ed
Natnae-l <131717014+Natnae-l@users.noreply.github.com>
2023-12-25 18:27:16 +0300

    new update

--- findTheMiddleElement.js
+++ findTheMiddleElement.js
@@ -12,6 +12,7 @@
 // gimme([5, 10, 14]) => 1
 // 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
 
+
 function gimme (triplet) {
     return triplet.findIndex(item => item != Math.max(...triplet) && item != Math.min(...triplet))
  }
